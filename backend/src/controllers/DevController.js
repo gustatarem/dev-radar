866f0c446543209bf61c81049b20368fc4139ac8
Gustavo <gustavo.tatarem@saedigital.com.br>
2020-01-15 09:25:54 -0300

    Created index method for listing users

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -2,31 +2,42 @@ const axios = require('axios');
 const Dev = require('../models/Dev');
 
 module.exports = {
+
+  async index(req, res) {
+    const devs = await Dev.find();
+
+    return res.json(devs);
+  },
+
   async store(req, res) {
     const { github_username, techs, latitude, longitude } = req.body;
 
-    const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+    let dev = await Dev.findOne({ github_username });
 
-    const { name = login, avatar_url, bio } = apiResponse.data;
+    if (!dev) {
+      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
-    const techsArray = techs.split(',').map(tech => tech.trim());
+      const { name = login, avatar_url, bio } = apiResponse.data;
 
-    const location = {
-      type: 'Point',
-      coordinates: [longitude, latitude]
-    };
+      const techsArray = techs.split(',').map(tech => tech.trim());
 
-    const dev = await Dev.create({
-      github_username,
-      name,
-      avatar_url,
-      bio,
-      techs: techsArray,
-      location
-    });
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude]
+      };
 
-    console.log(name, avatar_url, bio, github_username, techsArray);
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location
+      });
 
-    return res.json(dev);
+      return res.json(dev);
+    } else {
+      return res.status(400).send('O usuário já existe!');
+    }
   }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -8,5 +8,6 @@ routes.get('/', (req, res) => {
 });
 
 routes.post('/devs', DevController.store);
+routes.get('/devs', DevController.index);
 
 module.exports = routes;
