198f3752bd434c2771000f31b2a9a1ebfb7c6fa5
Gustavo <gustavo.tatarem@saedigital.com.br>
2020-01-15 09:00:17 -0300

    Storing location on database

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -9,7 +9,7 @@ routes.get('/', (req, res) => {
 });
 
 routes.post('/devs', async (req, res) => {
-  const { github_username, techs } = req.body;
+  const { github_username, techs, latitude, longitude } = req.body;
 
   const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
 
@@ -17,12 +17,18 @@ routes.post('/devs', async (req, res) => {
 
   const techsArray = techs.split(',').map(tech => tech.trim());
 
+  const location = {
+    type: 'Point',
+    coordinates: [longitude, latitude]
+  };
+
   const dev = await Dev.create({
     github_username,
     name,
     avatar_url,
     bio,
     techs: techsArray,
+    location
   });
 
   console.log(name, avatar_url, bio, github_username, techsArray);
