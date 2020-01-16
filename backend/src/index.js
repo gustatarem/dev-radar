99e932a1fea929e01565338ae74ceee04b0110f9
Gustavo <gustavo.tatarem@saedigital.com.br>
2020-01-15 22:25:23 -0300

    added library for accessing backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,6 +10,7 @@ mongoose.connect('mongodb+srv://tatarem:admin@cluster0-sojvp.mongodb.net/week10?
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
